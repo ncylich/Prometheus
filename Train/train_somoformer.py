c3657f81724327165134e149e281caeddb682452
Noah Cylich <noahcylich@gmail.com>
2024-11-07 02:13:31 -0800

    fixed dynamic path updating in get_data_loaders

--- Train/train_somoformer.py
+++ Train/train_somoformer.py
@@ -213,8 +213,11 @@ def get_data_loaders(backcast_size, forecast_size, test_size_ratio=.2, batch_siz
 
     # Updating data path dynamically on dir
     path_dirs = os.getcwd().split('/')[::-1]
-    path_dirs = [i for i, x in enumerate(path_dirs) if x == 'Prometheus']
-    prometheus_idx = path_dirs[0] if path_dirs else -1
+    prometheus_idx = -1
+    for i, dir in enumerate(path_dirs):
+        if dir == 'Prometheus':
+            prometheus_idx = i
+            break
     dataset_path = '../' * (prometheus_idx + 1) + dataset_path
 
     data = pd.read_csv(dataset_path)
