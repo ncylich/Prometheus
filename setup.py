e9cf2550d3f4140011a4501064a787b0499fae88
Noah Cylich <noahcylich@gmail.com>
2024-11-06 16:10:27 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'pandas',
+        'matplotlib',
+        'tqdm',
+        'pyarrow'
+    ],
+    entry_points={
+        'console_scripts': [
+            'train_model=Train.train_somoformer:main',
+            # 'squeeze_data=DataCollection.squezzing_data:main'
+        ],
+    },
+    author='Noah Cylich',
+    author_email='noahcylich@gmail.com',
+    description='Prometheus Quant Research',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ncylich/Prometheus',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
