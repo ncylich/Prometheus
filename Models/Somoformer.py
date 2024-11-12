2c02d34e97675f967a2dfb1564ba8e16486be954
ammesatyajit <ammesatyajit@gmail.com>
2024-11-12 00:06:42 -0800

    messing with hparams

--- Models/Somoformer.py
+++ Models/Somoformer.py
@@ -30,12 +30,12 @@ import matplotlib.pyplot as plt
 forecast_size = 36
 backcast_size = forecast_size * 2
 
-factor = 2
+factor = 4
 seq_len = backcast_size + forecast_size
 nhid = 128 * factor
 nhead = 8
 dim_feedfwd = 512 * factor
-nlayers = 12
+nlayers = 6
 dropout = 0.1
 batch_size = 1024
 test_col = 'close'
