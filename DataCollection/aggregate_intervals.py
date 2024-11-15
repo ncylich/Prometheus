bf372fbc14c7f7d3a478b12a4ae0788e482cec25
Noah Cylich <noahcylich@gmail.com>
2024-11-14 22:33:10 -0800

    added new time-series aggregating

---
+++ DataCollection/aggregate_intervals.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import long_term_data
+from tqdm import tqdm
+
+'''
+Given a df of the form: date,CL_open,CL_high,CL_low,CL_close,CL_volume,SI_open,SI_high,SI_low,SI_close,SI_volume,NG_open,NG_high,NG_low,NG_close,NG_volume,GC_open,GC_high,GC_low,GC_close,GC_volume,HG_open,HG_high,HG_low,HG_close,HG_volume,DX_open,DX_high,DX_low,DX_close,DX_volume,VX_open,VX_high,VX_low,VX_close,VX_volume,ZN_open,ZN_high,ZN_low,ZN_close,ZN_volume
+
+With 1 min intervals for each row, aggregate the data into 5 minute intervals.
+'''
+
+
+def process_col(col, col_name):
+    if 'date' in col_name:
+        return col.iloc[0]
+    if 'open' in col_name:
+        return col.iloc[0]
+    if 'high' in col_name:
+        return col.max()
+    if 'low' in col_name:
+        return col.min()
+    if 'close' in col_name:
+        return col.iloc[-1]
+    if 'volume' in col_name:
+        return col.sum()
+
+
+def aggregate_intervals(df, saved_csv, interval=5):
+    """
+    Aggregate data into intervals:
+        output directly into a csv instead of df to save memory and time for large datasets
+    """
+    cols = df.columns
+    with open(saved_csv, 'w') as f:
+        f.write(','.join(cols) + '\n')
+        for i in tqdm(range(0, len(df) - (interval - 1), interval)):
+            int_rows = df.iloc[i: i + interval]
+            new_row = [process_col(int_rows[col], col) for col in cols]
+            new_line = ','.join([str(x) for x in new_row]) + '\n'
+            f.write(new_line)
+
+
+def main():
+    print('Aggregating UNadjusted 1 min data into 5 min intervals')
+    df = pd.read_csv('long_term_data/1min_long_term_merged_UNadjusted.csv', dtype=long_term_data.COLS)
+    df['date'] = pd.to_datetime(df['date'], utc=True)
+    df['date'] = df['date'].dt.tz_convert('America/New_York')
+    aggregate_intervals(df, 'long_term_data/5_min_long_term_merged_UNadjusted.csv')
+
+    print('Aggregating adjusted 1 min data into 5 min intervals')
+    df = pd.read_csv('long_term_data/1min_long_term_merged_adjusted.csv', dtype=long_term_data.COLS)
+    df['date'] = pd.to_datetime(df['date'], utc=True)
+    df['date'] = df['date'].dt.tz_convert('America/New_York')
+    aggregate_intervals(df, 'long_term_data/5min_long_term_merged_adjusted.csv')
+
+if __name__ == '__main__':
+    main()
---
+++ DataCollection/long_term_data.py
@@ -0,0 +1,121 @@
+import os
+import sys
+import pandas as pd
+import pyarrow as pq
+
+
+COLS = {
+    'date': 'str',
+    'open': 'float32',
+    'high': 'float32',
+    'low': 'float32',
+    'close': 'float32',
+    'volume': 'int32'
+}
+
+unadjusted_postfix = '_full_1min_continuous_UNadjusted'
+adjusted_postfix = '_full_1min_continuous_ratio_adjusted'
+data_dir = 'long_term_data'
+
+def get_files(path=data_dir):
+    """
+    Get all files in a directory
+    """
+    all_files = os.listdir(path)
+    data_files = [f for f in all_files if unadjusted_postfix in f or adjusted_postfix in f]
+    data_files = add_cols(data_files)
+    return data_files
+
+def add_cols(data_files):
+    '''
+    Prepares the data files by adding the columns to the files that are missing them and correcting ending
+    '''
+    cols = ','.join(COLS.keys()) + '\n'
+    new_files = []
+    for f in data_files:
+        if not f.endswith('.txt'):
+            new_files.append(f)
+            continue
+        rename_only = True
+        with open(os.path.join(data_dir, f), 'r') as file:
+            lines = file.readlines()
+            if lines[0] != cols:
+                lines = [cols] + lines
+                rename_only = False
+
+        new_file_name = f[:-4] + '.csv'
+        if rename_only:
+            os.rename(os.path.join(data_dir, f), os.path.join(data_dir, new_file_name))
+        else:
+            with open(os.path.join(data_dir, new_file_name), 'w') as file:
+                file.writelines(lines)
+            os.remove(os.path.join(data_dir, f))
+        new_files.append(new_file_name)
+
+    return new_files
+
+def get_ticker(file):
+    """
+    Get ticker from file
+    """
+    return file.split('_')[0]
+
+def get_tickers(files):
+    """
+    Get tickers from files
+    """
+    tickers = set()
+    for file in files:
+        tickers.add(get_ticker(file))
+    return tickers
+
+def load_df(file):
+    """
+    Load dataframe from file
+    """
+    df = pd.read_csv(os.path.join(data_dir, file), dtype=COLS)
+    df['date'] = pd.to_datetime(df['date'], utc=True)
+    df['date'] = df['date'].dt.tz_convert('America/New_York')
+
+    ticker = get_ticker(file)
+    cols = set(df.columns) - {'date'}
+    cols = {col: f"{ticker}_{col.strip()}" for col in cols}
+    df = df.rename(columns=cols)
+
+    return df
+
+def merge_data(data_files, adjusted=False):
+    """
+    Merge data files
+    """
+    if adjusted:
+        data_files = [f for f in data_files if adjusted_postfix in f]
+    else:
+        data_files = [f for f in data_files if unadjusted_postfix in f]
+
+    # CL is the first file and first cols in the merged dataframe
+    cl_str = [f for f in data_files if 'CL' in f][0]
+    cl_idx = data_files.index(cl_str)
+    data_files = [data_files[cl_idx]] + data_files[:cl_idx] + data_files[cl_idx + 1:]
+
+    dfs = [load_df(file) for file in data_files]
+    merged_df = dfs[0]
+    for stock_df in dfs[1:]:
+        merged_df = merged_df.merge(stock_df, on=['date'], how='inner')
+    return merged_df
+
+def main():
+    files = get_files()
+
+    df = merge_data(files)
+    print('UNadjusted number of row:', len(df))
+    path = os.path.join(data_dir, '1min_long_term_merged_UNadjusted.csv')
+    df.to_csv(path, index=False)
+
+    df = merge_data(files, adjusted=True)
+    print('adjusted number of row:', len(df))
+    path = os.path.join(data_dir, '1min_long_term_merged_adjusted.csv')
+    df.to_csv(path, index=False)
+
+if __name__ == '__main__':
+    main()
